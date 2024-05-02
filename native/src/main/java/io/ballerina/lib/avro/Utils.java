5e98e01240dcb80ec3a279bc3dd0de7b5bb3b3e6
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2024-05-02 16:56:15 +0530

    Update APIs to support new modifications

--- native/src/main/java/io/ballerina/lib/avro/Utils.java
+++ native/src/main/java/io/ballerina/lib/avro/Utils.java
@@ -21,6 +21,9 @@ package io.ballerina.lib.avro;
 import io.ballerina.runtime.api.creators.ErrorCreator;
 import io.ballerina.runtime.api.utils.StringUtils;
 import io.ballerina.runtime.api.values.BError;
+import org.apache.avro.Schema;
+
+import java.util.List;
 
 import static io.ballerina.lib.avro.ModuleUtils.getModule;
 
@@ -31,11 +34,27 @@ public final class Utils {
 
     public static final String AVRO_SCHEMA = "avroSchema";
     public static final String ERROR_TYPE = "Error";
-    public static final String JSON_PROCESSING_ERROR = "JSON processing error";
-    public static final String DESERIALIZATION_ERROR = "Unable to deserialize the byte value";
+    public static final String SERIALIZATION_ERROR = "Avro serialization error";
+    public static final String DESERIALIZATION_ERROR = "Avro deserialization error";
+    public static final String STRING_TYPE = "BStringType";
+    public static final String ARRAY_TYPE = "BArrayType";
+    public static final String MAP_TYPE = "BMapType";
+    public static final String RECORD_TYPE = "BRecordType";
+    public static final String INTEGER_TYPE = "BIntegerType";
 
     public static BError createError(String message, Throwable throwable) {
         BError cause = ErrorCreator.createError(throwable);
         return ErrorCreator.createError(getModule(), ERROR_TYPE, StringUtils.fromString(message), cause, null);
+    }
+
+    public static Schema checkType(Schema.Type givenType, List<Schema> schemas) {
+        for (Schema schema: schemas) {
+            if (schema.getType().equals(Schema.Type.UNION)) {
+                checkType(givenType, schema.getTypes());
+            } else if (schema.getType().equals(givenType)) {
+                return schema;
+            }
+        }
+        return null;
     }
 }
